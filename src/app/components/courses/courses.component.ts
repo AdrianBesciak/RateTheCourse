4bc12fbb456aef3f03f560dec7ad0dd4a3e588a3
Adrian Beściak <adrian.besciak99@gmail.com>
2019-12-09 01:28:45 +0000

    fixed bugs in courses.component.ts

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../../models/Course';
-impor { Course } from '../../models/Course'
 
 @Component({
   selector: 'app-courses',
@@ -15,114 +14,114 @@ export class CoursesComponent implements OnInit {
   ngOnInit() {
     this.courses = [
       {
-        name = 'Wstęp do Informatyki';
-        ects = 6;
-        semester = 1;
-        lecture = true;
-        exercises = true;
-        laboratory = false;
-        project = false;
-        maxAmountOfStudents = 300;
-        courseRate = 1;
+        name : 'Wstęp do Informatyki',
+        ects : 6,
+        semester : 1,
+        lecture : true,
+        exercises : true,
+        laboratory : false,
+        project : false,
+        maxAmountOfStudents : 300,
+        courseRate : 1
       },
       {
-        name = 'Wprowadzenie do systemu UNIX';
-        ects = 4;
-        semester = 1;
-        lecture = true;
-        exercises = false;
-        laboratory = true;
-        project = false;
-        maxAmountOfStudents = 300;
-        courseRate = 2;
+        name : 'Wprowadzenie do systemu UNIX',
+        ects : 4,
+        semester : 1,
+        lecture : true,
+        exercises : false,
+        laboratory : true,
+        project : false,
+        maxAmountOfStudents : 300,
+        courseRate : 2
       },
       {
-        name = 'Kompetencje interpersonalne';
-        ects = 3;
-        semester = 1;
-        lecture = true;
-        exercises = true;
-        laboratory = false;
-        project = false;
-        maxAmountOfStudents = 300;
-        courseRate = 3;
+        name : 'Kompetencje interpersonalne',
+        ects : 3,
+        semester : 1,
+        lecture : true,
+        exercises : true,
+        laboratory : false,
+        project : false,
+        maxAmountOfStudents : 300,
+        courseRate : 3
       },
       {
-        name = 'Analiza matematyczna 1';
-        ects = 6;
-        semester = 1;
-        lecture = true;
-        exercises = true;
-        laboratory = false;
-        project = false;
-        maxAmountOfStudents = 300;
-        courseRate = 4;
+        name : 'Analiza matematyczna 1',
+        ects : 6,
+        semester : 1,
+        lecture : true,
+        exercises : true,
+        laboratory : false,
+        project : false,
+        maxAmountOfStudents : 300,
+        courseRate : 4
       },
       {
-        name = 'Algebra';
-        ects = 6;
-        semester = 1;
-        lecture = true;
-        exercises = true;
-        laboratory = false;
-        project = false;
-        maxAmountOfStudents = 300;
-        courseRate = 5;
+        name : 'Algebra',
+        ects : 6,
+        semester : 1,
+        lecture : true,
+        exercises : true,
+        laboratory : false,
+        project : false,
+        maxAmountOfStudents : 300,
+        courseRate : 5
       },
       {
-        name = 'Matematyka dyskretna';
-        ects = 5;
-        semester = 1;
-        lecture = true;
-        exercises = true;
-        laboratory = false;
-        project = false;
-        maxAmountOfStudents = 300;
-        courseRate = 1;
+        name : 'Matematyka dyskretna',
+        ects : 5,
+        semester : 1,
+        lecture : true,
+        exercises : true,
+        laboratory : false,
+        project : false,
+        maxAmountOfStudents : 300,
+        courseRate : 1
       },
       {
-        name = 'Algorytmy i struktury danych';
-        ects = 6;
-        semester = 1;
-        lecture = true;
-        exercises = true;
-        laboratory = false;
-        project = false;
-        maxAmountOfStudents = 300;
-        courseRate = 2;
+        name : 'Algorytmy i struktury danych',
+        ects : 6,
+        semester : 1,
+        lecture : true,
+        exercises : true,
+        laboratory : false,
+        project : false,
+        maxAmountOfStudents : 300,
+        courseRate : 2
       },
       {
-        name = 'Programowanie imperatywne';
-        ects = 5;
-        semester = 1;
-        lecture = true;
-        exercises = false;
-        laboratory = true;
-        project = false;
-        maxAmountOfStudents = 300;
-        courseRate = 3;
+        name : 'Programowanie imperatywne',
+        ects : 5,
+        semester : 1,
+        lecture : true,
+        exercises : false,
+        laboratory : true,
+        project : false,
+        maxAmountOfStudents : 300,
+        courseRate : 3
       },
       {
-        name = 'Fizyka 1';
-        ects = 6;
-        semester = 1;
-        lecture = true;
-        exercises = true;
-        laboratory = false;
-        project = false;
-        maxAmountOfStudents = 300;
-        courseRate = 4;
+        name : 'Fizyka 1',
+        ects : 6,
+        semester : 1,
+        lecture : true,
+        exercises : true,
+        laboratory : false,
+        project : false,
+        maxAmountOfStudents : 300,
+        courseRate : 4
       },
       {
-        name = 'Ochrona własności intelektualnej';
-        ects = 3;
-        semester = 1;
-        lecture = true;
-        exercises = false;
-        laboratory = false;
-        project = false;
-        maxAmountOfStudents = 300;
-        courseRate = 5;
+        name : 'Ochrona własności intelektualnej',
+        ects : 3,
+        semester : 1,
+        lecture : true,
+        exercises : false,
+        laboratory : false,
+        project : false,
+        maxAmountOfStudents : 300,
+        courseRate : 5
       }
     ]
   }
