37fa20c83692e6ce606ed9663087cf4ecde5ac8e
Adrian Beściak <adrian.besciak99@gmail.com>
2019-12-09 02:05:03 +0000

    showed course components on the website

--- src/app/components/course/course.component.ts
+++ src/app/components/course/course.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Course } from '../../models/Course';
 
 @Component({
   selector: 'app-course',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
+  @Input() course: Course;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  //set Dynamic Classes
+  setClasses() {
+    console.log('set classes');
+  }
+
+  onToggle(course) {
+    console.log('course opened');
+  }
+
+  onDelete(course) {
+    console.log('delete');
   }
 
 }
