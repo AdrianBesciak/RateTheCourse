8e5469ba8b0640eb74a36ec4500abd736a7f5332
Adrian Beściak <adrian.besciak99@gmail.com>
2020-01-06 20:03:44 +0000

    Course Details as Input

--- src/app/components/course-details/course-details.component.ts
+++ src/app/components/course-details/course-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Course } from '../../models/Course';
 
 @Component({
   selector: 'app-course-details',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-details.component.css']
 })
 export class CourseDetailsComponent implements OnInit {
+  @Input() course: Course;
 
   constructor() { }
 
