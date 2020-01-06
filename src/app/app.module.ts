bc10a5798730d962522672116782a4fd28bb3db6
Adrian Beściak <adrian.besciak99@gmail.com>
2020-01-06 19:58:13 +0000

    added Course Details to  Course Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './components/courses/courses.component';
 import { CourseComponent } from './components/course/course.component';
+import { CourseDetailsComponent } from './components/course-details/course-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    CourseComponent
+    CourseComponent,
+    CourseDetailsComponent
   ],
   imports: [
     BrowserModule,
