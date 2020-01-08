39daca014bdf2a420fc817601509ca098479b3f3
Adrian Beściak <adrian.besciak99@gmail.com>
2020-01-08 04:18:00 +0000

    Initialized Firebase in this app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './components/courses/courses.component';
@@ -11,6 +12,11 @@ import { CourseRatingComponent } from './components/course-rating/course-rating.
 import { HeaderComponent } from './components/header/header.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { RegisterComponent } from './components/register/register.component';
+
+//Firebase:
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -26,7 +32,9 @@ import { RegisterComponent } from './components/register/register.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAetBvQ-Z76ioivyClO30Y6IFuUSLtURr0",
+    authDomain: "ratethecourse-1b352.firebaseapp.com",
+    databaseURL: "https://ratethecourse-1b352.firebaseio.com",
+    projectId: "ratethecourse-1b352",
+    storageBucket: "ratethecourse-1b352.appspot.com",
+    messagingSenderId: "240258592899",
+    appId: "1:240258592899:web:085a68614768671f1eebb6"
+  }
 };
 
 /*
